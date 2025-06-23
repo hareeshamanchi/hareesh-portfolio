75cd261c461867554282692896a4cb12e72ab55f
Hareesh Amanchi <hareeshamanchi@gmail.com>
2025-06-23 22:30:28 +0530

    Add Navbar component for site navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
-import React from "react";
-import "./Navbar.css";
+import React, { useState } from 'react';
+import './Navbar.css';
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo">Hareesh 🚀</div>
-      <ul className="nav-links">
+
+      <div className={`menu-icon ${open ? "open" : ""}`} onClick={() => setOpen(!open)}>
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+
+      <ul className={`nav-links ${open ? "active" : ""}`}>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
